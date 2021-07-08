d97545af99b6cfc60d16e5f126f9dba7dde7622c
TakumiOtsuki-417 <heitaku443@gmail.com>
2021-07-08 20:03:38 +0900

    単体テストコード実装
    ・gem「RSpec」「FactoryBot」「Faker」「Gimei」の導入
    ・Userモデルの単体テストコード記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6, mix_case: true)
+    password {password}
+    password_confirmation {password}
+    nickname {Faker::Name.name}
+    last_name {Gimei.last.kanji}
+    first_name {Gimei.first.kanji}
+    last_name_kana {Gimei.last.katakana}
+    first_name_kana {Gimei.first.katakana}
+    birth {Faker::Date.between(from: '1995-09-23', to: '2014-09-25')}
+  end
+end
