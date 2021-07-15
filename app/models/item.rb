e9768f33c6fcacca39e282961ba86dcdc0b0ac9f
TakumiOtsuki-417 <heitaku443@gmail.com>
2021-07-15 13:55:05 +0900

    商品出品登録
    ・ルーティングとコントローラー記述
    ・データベース保存確認
    ・エラーハンドリング確認

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,20 +2,19 @@ class Item < ApplicationRecord
 
   # Validation
   with_options presence: true do
-    with_options numericality: { other_than: 1 } do
+    validates :image
+    validates :name
+    validates :description
+    validates :price, numericality: {only_integer: true, message: ' is invalid. Input half-width characters'}
+    with_options numericality: { other_than: 1, message: "can't be blank" } do
       validates :genre_id
       validates :status_id
       validates :shipping_day_id
       validates :delivery_charge_id
       validates :prefecture_id
     end
-    validates :image
-    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/
-, message: 'is invalid. Input half-width characters' }
   end
-    validates :name
-    validates :description
-  end
+    validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
 
   # Association
   has_one_attached :image
